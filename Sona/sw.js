b1e6c1f26dcc1d8bc70a51323cb71a0f695b80b4
meyjesjack <meyjesjack@gmail.com>
2026-03-29 20:09:41 -0700

    Update sw.js

--- Sona/sw.js
+++ Sona/sw.js
@@ -1,8 +1,6 @@
 const CACHE = "sona-v1";
-const ASSETS = ["/Sona/", "/Sona/index.html"];
 
 self.addEventListener("install", function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
   self.skipWaiting();
 });
 
@@ -14,10 +12,8 @@ self.addEventListener("activate", function(e){
 });
 
 self.addEventListener("fetch", function(e){
-  // Network first for API calls, cache first for assets
   if(e.request.url.indexOf("ticketmaster")>=0 || e.request.url.indexOf("anthropic")>=0 || e.request.url.indexOf("spotify")>=0){
-    e.respondWith(fetch(e.request).catch(function(){ return caches.match(e.request); }));
-  } else {
-    e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
+    return;
   }
+  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
 });
